79d0b4d01ce0cb83ba9ad9975ebdb952ad9b0783
arthur <arthurvdmerwe@me.com>
2017-08-21 19:11:53 +1000

    restructured seret sharing for use in elgamal

--- ElGamal.py
+++ ElGamal.py
@@ -4,16 +4,30 @@ from toolbox.PKEnc import PKEnc
 from schemes.pkenc_elgamal85 import ElGamal
 from toolbox.eccurve import prime192v2
 
+class Threshold_ElGamal:
 
+    def __init__(self):
+        self.el = ElGamal(ecc, prime192v2)
 
-el = ElGamal(ecc, prime192v2)
+    def keygen(self):
+        (pk, sk) = self.el.keygen()
+        self.pk = pk
+        self.sk = sk
 
-(pk, sk) = el.keygen()
+    def getPK(self):
+        return self.pk
 
-msg = b"hello world!"
-size = len(msg)
-cipher1 = el.encrypt(pk, msg)
+    def getSK(self):
+        return self.sk
 
-m = el.decrypt(pk, sk, cipher1)
-assert m == msg, "Failed Decryption!!!"
-print("SUCCESSFULLY DECRYPTED!!!")
+    def encrypt(self, message):
+        self.msg = message
+        size = len(self.msg)
+        cipher = self.el.encrypt(self.pk, self.msg)
+        return cipher
+
+    def decrypt(self, cipher):
+        m = self.el.decrypt(self.pk, self.sk, cipher)
+        assert m == self.msg, "Failed Decryption!!!"
+        print("SUCCESSFULLY DECRYPTED!!!")
+        return m
